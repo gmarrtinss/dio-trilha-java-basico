122c713750831f84a4068bea3ceaf4b6032ba05c
Guilherme Martins <97321294+gmarrtinss@users.noreply.github.com>
2024-05-28 01:22:29 -0400

    Update ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,13 +4,13 @@ public class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Definindo variáveiS
-        int numero;
+        int numeroConta;
         String agencia;
         String nomeCliente;
         double saldo;
         // Solicitando os dados ao usuário
         System.out.print("Por favor, digite o número da conta: ");
-        numero = scanner.nextInt();
+        numeroConta = scanner.nextInt();
         scanner.nextLine(); // Consumir o newline
 
         System.out.print("Por favor, digite o número da agência: ");
@@ -22,7 +22,7 @@ public class ContaTerminal {
         System.out.print("Por favor, digite o saldo inicial: ");
         saldo = scanner.nextDouble();
 
-        System.out.print("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de R$ " + saldo + " já está disponível para saque");
+        System.out.print("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo de R$ " + saldo + " já está disponível para saque");
 
         scanner.close();
     }
